dc143821529dafd6211962fdf76067632a9a17a5
Joao Batista de Oliveira <joboliveira@gmail.com>
2019-06-19 00:25:23 -0300

    Create signal_generator.c
    Some signal forms

---
+++ signal_generator.c
@@ -0,0 +1,39 @@
+#include <math.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include "signal_generator.h"
+
+#define PI 3.141592654
+/*
+typedef struct sirgnal_t{
+  float amplitude;
+  float frequency;
+  float time;
+  float phase;
+  float output;
+} SIGNAL_T;
+*/
+
+int senoidal(SIGNAL_T *this)
+{
+  SIGNAL_T *signal;
+  signal = this;
+  if(signal == NULL)
+  {
+    return 1;
+  }
+  signal->output = signal->amplitude*sin((2.0*(PI/180.0)*signal->frequency*signal->time)+signal->phase);
+  return 0;
+}
+
+int cosenoidal(SIGNAL_T *this)
+{
+  SIGNAL_T *signal;
+  signal = this;
+  if(signal == NULL)
+  {
+    return 1;
+  }
+  signal->output = signal->amplitude*sin((2*PI*signal->frequency*signal->time)+signal->phase);
+  return 0;
+}
