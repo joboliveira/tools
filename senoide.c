098695469505d4b448119da4b58d9eb562a34e31
João Batista de Oliveira <joboliveira@gmail.com>
2020-03-26 10:06:55 -0300

    Changes to be committed: 	modified:   senoide.c
    Changed some types on senoide.c for new generators:

--- senoide.c
+++ senoide.c
@@ -6,26 +6,30 @@
 
 /*
 *
-*   Observações Importantes.
+*   Observa��es Importantes.
 *
-*  Para um pulso de 0.5us é preciso de uma senoide / cossenoide com frequência de 2.000MHz
-*  Para um pulso de 1.0us é preciso de uma senoide / cossenoide com frequência de 1.000MHz
-*  Para um pulso de 2.0us é preciso de uma senoide / cossenoide com frequência de 0.500MHz
+*  Para um pulso de 0.5us é preciso de uma senoide / cossenoide com frequ�ncia de 2.000MHz
+*  Para um pulso de 1.0us é preciso de uma senoide / cossenoide com frequ�ncia de 1.000MHz
+*  Para um pulso de 2.0us é preciso de uma senoide / cossenoide com frequ�ncia de 0.500MHz
 *
-*  Ou, por simplificação, usamos a maior restrição, embora o número de amostras de um ciclo possa deteriorar o sinal.
+*  Ou, por simplifica��o, usamos a maior restri��o, embora o n�mero de amostras de um ciclo possa deteriorar o sinal.
 *  0.5us teremos 10 samples de um sinal de 2.0MHz, o que equivale a um ciclo completo do sinal.
 *  1.0us teremos 20 samples de um sinal de 2.0MHz, o que equivale a dois ciclos completos do sinal.
 *  2.0us teremos 40 samples de um sinal de 2.0MHz, o que equivale a dois ciclos completos do sinal.
 */
-#define PI        3.141592654
-float k;
-float f_sinal;
-float f_amostra;
+#define PI  3.141592654
+float 		k;
+float 		f_sinal;
+float 		f_amostra;
 
 
 int32_t n=0;
-long n_elem;
-float *y;
+long 	n_elem;
+long  n_carrier;
+float 	*y;
+char	FileName_Float[256];
+char	FileName_Int[256];
+char	FileName_HDF[256];
 
 
 FILE *arquivoFloat;
@@ -33,72 +37,88 @@ FILE *arquivoInt;
 
 int main(int argc, char **argv)
 {
- if(argc <= 4){
-  printf("type senoide <sin|cos> <freq_signal> <Sampling_rate> <samples>\r\n");
+ if(argc <= 5){
+  printf("type senoide <sin|cos> <freq_signal> <Sampling_rate> <samples> <n_carriers>\r\n");
   return -1;
  }
+ strcpy(FileName_Float,argv[1]);
+ strcat(FileName_Float,"_");
+ strcat(FileName_Float,argv[2]);
+ strcat(FileName_Float,"_");
+ strcat(FileName_Float,argv[3]);
+ strcat(FileName_Float,"_");
+ strcat(FileName_Float,argv[4]);
+ strcat(FileName_Float,"_Float.txt");
+
+ strcpy(FileName_Int,argv[1]);
+ strcat(FileName_Int,"_");
+ strcat(FileName_Int,argv[2]);
+ strcat(FileName_Int,"_");
+ strcat(FileName_Int,argv[3]);
+ strcat(FileName_Int,"_");
+ strcat(FileName_Int,argv[4]);
+ strcat(FileName_Int,"_Int.txt");
+
  printf("Gerador de onda senoidal em arquivo\r\n");
- printf("Arquivo de saida: SinalSenoidalFloat.txt\r\n");
- printf("Arquivo de saida: SinalSenoidalInt.txt\r\n");
- if((arquivoFloat =  fopen("SinalSenoidalFloat.txt","w")) == NULL)
+ printf("Arquivo de saida: %s\r\n",FileName_Float);
+ printf("Arquivo de saida: %s\r\n", FileName_Int);
+ if((arquivoFloat = fopen(FileName_Float,"w")) == NULL)
+ //if((arquivoFloat = fopen("SinalSenoidalFloat.txt","w")) == NULL)
  {
   printf("Erro ao abrir o arquivo.\r\n");
   return -1;
  }
 
- f_sinal  = atof(argv[2]);
- f_amostra = atof(argv[3]);
- k = (2 * cos(2 * PI * (f_sinal/f_amostra)));
- n_elem = atol(argv[4]);
- y = (float *) malloc(n_elem * sizeof(float));
+ f_sinal    = atof(argv[2]);
+ f_amostra	= atof(argv[3]);
+ //k          = (2 * cos(2 * PI * (f_sinal/f_amostra)));
+ n_elem     = atol(argv[4]);
+ n_carrier  = atol(argv[5]);
+ y          = (float *) malloc(n_elem * sizeof(float) * n_carrier);
 
- if((arquivoInt =  fopen("SinalSenoidalInt.txt","w")) == NULL)
+ if((arquivoInt =  fopen(FileName_Int,"w")) == NULL)
  {
   printf("Erro ao abrir o arquivo.\r\n");
   free(y);
   return -1;
  }
-
- //Constante
- if (strcmp(argv[1],"sin") == 0)
- {
-  y[0]=0.0;
- } else if (strcmp(argv[1],"cos") == 0)
- {
-  y[0]=1.0;
- }
- else
- {
-  free(y);
-  return -1;
- }
-
- fprintf(arquivoFloat, "%f\r\n",y[0]);
- fprintf(arquivoInt, "%d\r\n",(int16_t)(y[0]*2047));
 
 // Constante
+for(long carrier=0; carrier < n_carrier; carrier++)
+{
+  k = (2 * cos(2*PI*(f_sinal*(carrier+1)/f_amostra)));
  if (strcmp(argv[1],"sin") == 0)
  {
-	 y[1]=sin(2*PI*((float)f_sinal/(float)f_amostra));
+	 y[carrier + 0]=sin(0);
+	 y[carrier + 1]=sin(2*PI*((float)f_sinal*(carrier+1)/(float)f_amostra));
  }
  else if (strcmp(argv[1],"cos") == 0)
  {
-	 y[1]=cos(2*PI*((float)f_sinal/(float)f_amostra));
+	 y[carrier + 0]=cos(0);
+	 y[carrier + 1]=cos(2*PI*((float)f_sinal*(carrier+1)/(float)f_amostra));
  } else
  {
 	 free(y);
 	 return -1;
  }
 
-fprintf(arquivoFloat,"%f\r\n",y[1]);
-fprintf(arquivoInt,"%d\r\n",(int16_t)(y[1]*2047));
+
+ fprintf(arquivoFloat, "%f\r\n",y[carrier + 0]);
+ fprintf(arquivoFloat, "%f\r\n",y[carrier + 1]);
+
+ fprintf(arquivoInt, "%d\r\n",(int16_t)(y[carrier + 0]*2047));
+ fprintf(arquivoInt, "%d\r\n",(int16_t)(y[carrier + 1]*2047));
 
  for (n=2; n < n_elem; n++)
  {
-  y[n] = k*y[n-1]-y[n-2];
-  fprintf(arquivoFloat,"%f\r\n",y[n]);
-  fprintf(arquivoInt,"%d\r\n",(int16_t)(y[n]*2047));
+   //for(carrier=0; carrier < num_carrier; carrier++)
+   //{
+     y[carrier + n] = k*y[carrier + (n-1)]-y[carrier + (n-2)];
+     fprintf(arquivoFloat,"%f\r\n",y[carrier + n]);
+     fprintf(arquivoInt,"%d\r\n",(int16_t)(y[carrier + n]*2047));
+   //}
  }
+}
  fclose(arquivoFloat);
  fclose(arquivoInt);
  free(y);
